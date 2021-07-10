c0168275b7bf4ba98f2be314fb44758c432a384a
Bibek <ilovecoding643@gmail.com>
2021-07-10 18:31:35 +0545

    child commit

---
+++ vowelorNot.py
@@ -0,0 +1,14 @@
+# Write a program to check whether the input alphabet is vowel or not using if-else.
+
+alphabet = input("Enter the alphabet: ").lower()
+
+
+if alphabet in ('a','e','i','o','u') :
+    print("%s alphabet is vowel alphabet." %alphabet)
+else:
+    print(" it is not a vowel alphabet")
+
+
+
+
+
