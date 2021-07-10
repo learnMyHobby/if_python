9c7216c23fb7208990a8c6df08ac7355b419c4e8
Bibek <ilovecoding643@gmail.com>
2021-07-10 18:56:26 +0545

    Added other files

---
+++ leapYear.py
@@ -0,0 +1,16 @@
+# Write a program to check whether the entered year is leap year or not.
+
+# leap year means it has to be 366 days and it will occur once every four years
+
+year = int(input("Enter a year: "))
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
+
